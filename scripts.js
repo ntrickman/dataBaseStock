822e174c4e90e93c9e8f5c6259c90e9721387e6c
ntrickman <42870934+ntrickman@users.noreply.github.com>
2021-05-05 20:14:23 -0500

    Basic Form

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
 function gotoStock(){
     
 }
+
+function test() {          
+    var symbol = document.getElementsByName("symbol")[0].value;
+    var qty = document.getElementsByName("qty")[0].value;
+    var tradePrice = document.getElementsByName("tradePrice")[0].value;
+    var openDate = document.getElementsByName("openDate")[0].value;
+    // if (tradePrice == null) {
+    //     tradePrice = yahooStockPrices.getCurrentData(symbol);
+    // }
+    document.write("Symbol: ", symbol);
+    document.write("<br>Quantiy: ", qty);
+    document.write("<br>Trade Price: ", tradePrice);
+    document.write("<br>Dated Opened: ", openDate);
+    
+    
+}
